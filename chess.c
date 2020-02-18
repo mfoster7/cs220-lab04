aed778d3b07bb1d604a6f0be9eb9b62f7643c0e8
Matthew Foster <mfoster7@cs.binghamton.edu>
2020-02-18 17:57:55 -0500

    first commit

---
+++ chess.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "chess.h"
+
+uint64_t king_pos = 0;
+
+unsigned int make_move (Move m){
+	if(!IS_SET(king_pos, m)){
+		return 0;
+	}
+	if(m.to != NORTH_OF(m.from) || SOUTH_OF(m.from) || EAST_OF(m.from) ||
+	WEST_OF(m.from) || SE_OF(m.from) || SW_OF(m.from) || NE_OF(m.from) || NW_OF(m.from)){
+		return 0;
+	}
+	CLEAR_BIT(king_pos, m.from);
+	SET_BIT(king_pos, m.to);
+	return 1;
+}
+
+int main(){
+	char s[128];
+	uint8_t index;
+	unsigned int error;
+	Move m = {INVALID_INDEX, INVALID_INDEX};
+
+	printf("Enter initial king position: ");
+	fgets(s, 127, stdin);
+	index = notation_to_index(s[0], s[1]);
+	if(index == INVALID_INDEX){
+		printf("%d", error);
+		return 0;
+	}
+	SET_BIT(king_pos, index);
+
+	display_board();
+	
+	for(; ;){
+		m = get_move();
+	
+		if(make_move(m) != 1){
+			printf("Invalid move. Try again!\n");
+		}else{
+			display_board();
+		}
+	}
+	return 0;
+}
